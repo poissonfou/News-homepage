1a32767a8814c571ce1fd1ddac46ba339fea2fc2
Emerson Lima <emersonlimago123@gmail.com>
2024-10-16 19:08:29 -0300

    fixed responsivity

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const menuOpenBtn = document.querySelector('[aria-label="open menu"]');
 const menuCloseBtn = document.querySelector('[aria-label="close menu"]');
 const menuMobile = document.getElementById("nav-mobile");
+const overlay = document.getElementsByClassName("overlay")[0];
 
 menuOpenBtn.addEventListener("click", () => {
   menuMobile.style.transform = "translateX(0%)";
+  overlay.removeAttribute("hidden");
 });
 
 menuCloseBtn.addEventListener("click", () => {
   menuMobile.removeAttribute("style");
+  overlay.setAttribute("hidden", true);
 });
